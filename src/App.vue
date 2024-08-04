f0114552923a704c9a4b59926464761946763c11
Masion-coder <2408766306@qq.com>
2024-08-04 23:38:20 +0800

    vue-setup返回值

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <Person/>
+    <h1>1234</h1>
+</template>
+
+<script>
+    import Person from './components/Person.vue';
+
+
+    export default {
+        name:'App',
+        components:{Person}
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="person">
+        <h1>姓名:{{ name }}</h1>
+        <h1>年龄:{{ age }}</h1>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="showTel">查看联系方式</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Person',
+    setup() {
+        // 数据，不是响应式数据
+        let name = 'Masion'
+        let age = 18
+        let tel = '12345678'
+
+        function changeAge() {
+
+        }
+
+        function showTel() {
+            alert(tel)
+        }
+
+        return { name, age, changeAge, showTel }
+    }
+}
+</script>
