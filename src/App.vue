61bdbb4ca8cf31d6d4425feac6b1fde2c07d2be6
Masion-coder <2408766306@qq.com>
2024-08-08 20:51:16 +0800

    vue-标签ref

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
-    <WatchEffect/>
+    <h1 ref="title">6789</h1>
+    <button @click="show">bbb</button>
+    <Person ref="person"/>
+    <button @click="showPerson">人</button>
 </template>
-
 <script>
-    import Car from './components/Car.vue';
+export default {
+    name: 'App'
+}
+</script>
+<script setup>
+import Car from './components/Car.vue';
 import Person from './components/Person.vue';
 import Watch from './components/Watch.vue';
 import WatchEffect from './components/WatchEffect.vue';
+import { ref } from 'vue'
 
+let title = ref()
+let person = ref()
 
-    export default {
-        name:'App',
-        components:{WatchEffect}
-    }
+function show() {
+    console.log(title.value)
+}
+
+function showPerson() {
+    // 返回组件实例
+    console.log(person.value)
+}
+
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="person">
-        性：<input type="text" v-model="firstName"> <br>
-        名：<input type="text" v-model="lastName"> <br>
-        全名：<span>{{ fullName }}</span> <br>
-        <button @click="changeFullName">修改全称</button>
+        <h1 ref="title">123456</h1>
+        <button @click="show">aaa</button>
     </div>
 </template>
 
@@ -14,27 +12,12 @@ export default {
 </script>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 
-let firstName = ref('张')
-let lastName = ref('三')
+let title = ref()
 
-// // 计算属性，只读，不能改
-// let fullName = computed(() => { return firstName.value + '-' + lastName.value })
-
-
-let fullName = computed({
-    get() {
-        return firstName.value + '-' + lastName.value
-    },
-    set(val) {
-        const [na1,na2] = val.split('-')
-        firstName.value = na1
-        lastName.value = na2
-    }
-})
-
-function changeFullName() {
-    fullName.value = 'li-si'
+function show() {
+    console.log(title.value)
 }
+
 </script>
